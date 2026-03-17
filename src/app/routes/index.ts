e6d15c7a99f6eef72759fa1731db906bc53c7696
refatalhasankaif <refatalhasank@protonmail.com>
2026-03-18 05:52:09 +0600

    payment module need to create

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,7 +4,7 @@ import { userRoutes }      from '../modules/user/user.routes';
 import { urlRoutes }       from '../modules/urls/urls.routes';
 import { adminRoutes }     from '../modules/admin/admin.routes';
 import { analyticsRoutes } from '../modules/analytics/analytics.routes';
-import { paymentRoutes } from '../modules/payment/payments.routes';
+// import { paymentRoutes } from '../modules/payment/payments.routes';
 
 const router = Router()
 
@@ -13,6 +13,6 @@ router.use('/users',     userRoutes)
 router.use('/urls',      urlRoutes)
 router.use('/admin',     adminRoutes)
 router.use('/analytics', analyticsRoutes)
-router.use('/payments',  paymentRoutes)
+// router.use('/payments',  paymentRoutes)
 
 export const IndexRoutes = router
